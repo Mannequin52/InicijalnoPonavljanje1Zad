9ae57573743766522015f3c074966d9328d0d57b
Mannequin52 <92926451+Mannequin52@users.noreply.github.com>
2023-09-11 10:31:14 +0200

    Add files via upload

---
+++ Inicijalni_4D/Inicijalni_4D/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inicijalni_4D
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Napišite program u kojem ćete deklarirati varijable a i b tipa int,
+             * te im dodijelite vrijednosti 100 (a) i 3 (b). Ispišite njihove vrijednosti
+             * u sljedećem obliku: “a dijeljeno s b je x i ostatak y”*/
+
+            int a = 100;
+            int b = 3;
+            Console.WriteLine("a dijeljeno s b je " + a/b + "i ostatak " + a%b);
+            Console.ReadKey();
+        }
+    }
+}
